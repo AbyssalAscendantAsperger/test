a72a7bd5616b07c2c726cd212be36b7d8cfc939b
AbyssalAscendantAsperger <ngan987654321bach@gmail.com>
2026-06-28 19:40:44 +0700

    Remove obsolete runtime components: object, string, throwable, scheduler, thread, and vm-executor files; update TypeScript configuration.

--- java/web5/src/game-modules/stdlib/javax.microedition/lcdui/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '../../../ui/Display';
-export * from '../../../ui/Displayable';
-export * from '../../../ui/Canvas';
-export * from '../../../graphics/Graphics';
-export * from '../../../graphics/Image';
-export * from '../../../graphics/Font';
